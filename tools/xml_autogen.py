a7294f36e8b5a1f13ae42eceed3c47d5a3161fd3
ThirdAmetistEye <nn0uneim@yandex.ru>
2023-07-11 13:18:46 +0300

    built-in autogeneration of the manipulator

--- tools/xml_autogen.py
+++ tools/xml_autogen.py
@@ -7,6 +7,41 @@ def collision_section(joints, obstacles):
             if j - i > 1:
                 f.write(' ' * 8 + f'<pair geom1="geom edge {i}" geom2="geom edge {j}"/>\n')
 
+def manipulator_autogen(joints, size, pos):
+    output = ''
+    output_suffix = []
+    output_prefix = ''
+    current_space = '        '
+    for i in range(joints):
+        if i == 0:
+            euler = '\"0 90 0\"'
+        else:
+            euler = '\"0 0 0\"'
+        output_prefix += f'{current_space}<body name=\"edge {i}\" pos={pos[f"edge_{i}"]} euler={euler}>\n'
+        
+        output_prefix += f'{current_space + "    "}<joint name=\"joint {i}\" type=\"hinge\" axis=\"-1 0 0\" pos={pos["joints"]}/>\n'
+        output_prefix += f'{current_space + "    "}<geom name=\"geom edge {i}\" type=\"cylinder\" size={size["cylinder"]} material=\"blue\"/>\n'
+        if i == joints - 1:
+            output_prefix += f'{current_space + "    "}<site name=\"tip\" size={size["tip"]} pos={pos["tip"]}/>\n'
+        output_suffix.append(f'{current_space}</body>\n')
+        current_space += '    '
+    output += output_prefix + ''.join(output_suffix[-1:-len(output_suffix)-1:-1]) + '\n'
+    current_space = '        '
+    output_prefix = ''
+    output_suffix = []
+    for i in range(joints):
+        if i == 0:
+            euler = '\"0 90 0\"'
+        else:
+            euler = '\"0 0 0\"'
+        output_prefix += f'{current_space}<body name=\"edge {i} shade\" pos={pos[f"edge_{i}"]} euler={euler}>\n'
+        output_prefix += f'{current_space + "    "}<joint name=\"joint {i} shade\" type=\"hinge\" axis=\"-1 0 0\" pos={pos["joints"]}/>\n'
+        output_prefix += f'{current_space + "    "}<geom name=\"geom edge {i} shade\" type=\"cylinder\" size={size["cylinder"]} material=\"green\"/>\n'
+        output_suffix.append(f'{current_space}</body>\n')
+        current_space += '    '
+    output += output_prefix + ''.join(output_suffix[-1:-len(output_suffix)-1:-1])
+    return output
+
 joints = 2
 obstacles = 6
 
@@ -16,7 +51,8 @@ file_path_to_obstacles = 'tools/obstacles.txt'
 with open(f'tools/{joints}-dof_{obstacles}-obs_manipulator.xml', "w+") as f:
     f.write(open('tools/const/header.txt', 'r').read())
     f.write('\n')
-    f.write(open(file_path_to_edges, 'r').read())
+    f.write(manipulator_autogen(joints, {"cylinder":'\"0.5 0 0.1\"', \
+    "tip":'\"0.1\"'}, {"edge_0":'\"0.5 0 0.1\"', "edge_1":'\"0 0 1\"', "joints":'\"0 0 -0.5\"', "tip":'\"0 0 0.4\"'}))
     f.write('\n')
     f.write(open(file_path_to_obstacles, 'r').read())
     f.write('\n')
